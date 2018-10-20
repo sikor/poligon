ac8fd5676e7c3117e772c48770d97449a751fb52
sikor <siegfriedek@gmail.com>
2018-10-20 15:53:19 +0200

    Fixed test, using Seq instead of Vector :/

--- poligon-vaadin/src/test/scala/UdashPropertiesTest.scala
+++ poligon-vaadin/src/test/scala/UdashPropertiesTest.scala
@@ -1,7 +1,5 @@
 import io.udash.properties.HasModelPropertyCreator
-import io.udash.properties.model.ModelProperty
 import io.udash.properties.seq.SeqProperty
-import io.udash.properties.single.CastableProperty
 import org.scalatest.FunSuite
 
 class UdashPropertiesTest extends FunSuite {
@@ -19,24 +17,25 @@ class UdashPropertiesTest extends FunSuite {
 
   //  object MultiResource extends HasModelPropertyCreator[MultiResource]
 
-  case class ObjectInstance(id: Int, resources: Vector[Resource])
+  case class ObjectInstance(id: Int, resources: Seq[Resource])
 
   object ObjectInstance extends HasModelPropertyCreator[ObjectInstance]
 
-  case class SomeObject(name: String, instances: Vector[ObjectInstance])
+  case class SomeObject(name: String, instances: Seq[ObjectInstance])
 
   object SomeObject extends HasModelPropertyCreator[SomeObject]
 
 
   test("Property binding") {
     val model =
-      ModelProperty(
+      SeqProperty(
         SomeObject("object 1", Vector(
           ObjectInstance(3, Vector(
             SingleResource("resource 1", "value1"),
             MultiResource("multi resource", Map(2 -> "value 2", 3 -> "value 3")))))))
 
-    val instances = model.subSeq(_.instances)
+    val instances = model.elemProperties.find(o => o.get.name == "object 1")
+      .get.asModel.subSeq(_.instances)
     val resourceModel = instances.elemProperties.find(p => p.get.id == 3).get.asModel
       .subSeq(_.resources).elemProperties.find(p => p.get.name == "resource 1").get
 
