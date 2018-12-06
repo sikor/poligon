de5e52b4d245d61011b2a3d6d760ed81abe99ccd
sikor <siegfriedek@gmail.com>
2018-12-07 00:28:27 +0100

    Next plans

--- poligon-vaadin/src/main/scala/poligon/polyproperty/PropertyChanger.scala
+++ poligon-vaadin/src/main/scala/poligon/polyproperty/PropertyChanger.scala
@@ -3,6 +3,12 @@ package poligon.polyproperty
 import poligon.polyproperty.PropertyMarker.MarkedProperties
 import poligon.polyproperty.PropertyObserver.ObservedProperties
 
+/**
+  * TODO:
+  * 1. Add possibility to amend list without overwriting all values
+  * 2. Use polyproperty instead of io.udash in example vaadin project
+  * 3. Consider defining special property for Presenters -> Property that holds something that holds another property (flatMap?)
+  */
 object PropertyChanger {
   def set[T: PropertyCodec](property: PropertyWithParent[T], value: T)(implicit observed: ObservedProperties): Unit = {
     val mp = new MarkedProperties()
